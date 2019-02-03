be54625290c2b7a89c41cc3628ef1e4eee66133f
Pichet Fueangfu <28233224+N0SPichet@users.noreply.github.com>
2019-02-03 21:31:23 +0700

    relations created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+	protected $fillable = ['name', 'detail', 'price', 'stock', 'discount'];
+
+    public function reviews()
+    {
+    	return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+	protected $fillable = ['product_id', 'customer', 'review', 'star'];
+
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
